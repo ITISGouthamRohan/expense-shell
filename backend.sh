c8185557579492efd62b063b07967f68f2b5f38c
ITISGouthamRohan <gouthamrohan1@gmail.com>
2025-05-04 13:45:54 +0530

    Expense Shell Project

--- backend.sh
+++ backend.sh
@@ -46,5 +46,5 @@
     useradd expense &>>$LOGFILE
     VALIDATE $? "Creating expense user"
  else
-     echo "Expense user already created...$y SKIPPING $N"
+     echo "Expense user already created...$Y SKIPPING $N"
  fi    
